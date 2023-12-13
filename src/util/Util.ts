57677549b476e0f5d0912ff252f851881fc3129d
Cyberghxst <jeffreyvillatoroquezada@gmail.com>
2023-12-13 11:28:42 -0600

    Added more Util methods

--- src/util/Util.ts
+++ src/util/Util.ts
@@ -1,4 +1,6 @@
-import { Guild, PermissionsBitField, PermissionsString } from 'discord.js'
+import { Guild, GuildTextBasedChannel, PermissionsBitField, PermissionsString, Snowflake, User } from 'discord.js'
+import { Bot } from 'src/structures/Bot'
+import { Context } from 'src/structures/Context'
 
 export class Util {
     /**
@@ -128,6 +130,19 @@ export class Util {
     }
 
     /**
+     * Get a message.
+     * @param channel - Channel to get the message from.
+     * @param query - Message ID.
+     * @returns {Promise<Message<true> | null>}
+     */
+    static async getMessage(channel: GuildTextBasedChannel, query: string) {
+        if (!query) return null
+        const someId = query.replace(/[^\d]/g, '')
+        let message = someId ? channel.messages.cache.get(someId) || (await channel.messages.fetch(someId).catch(e => null)) : null
+        return message
+    }
+
+    /**
      * Get a guild role.
      * @param {string} query - Role resolver.
      * @param {Guild} guild - The guild where role is in.
@@ -139,5 +154,33 @@ export class Util {
         let role = someId ? guild.roles.cache.get(someId) || (await guild.roles.fetch(someId).catch(e => null)) : null
         if (!role) role = guild.roles.cache.find(r => r.name.includes(query)) || null
         return role
+    }
+
+    /**
+     * Get an user.
+     * @param bot - BDJS client.
+     * @param id - User ID.
+     * @param strict - Set to "true" if id is resolvable.
+     * @returns {Promise<User | null>}
+     */
+    static async getUser(bot: Bot, id: Snowflake, strict: boolean = false) {
+        if(!id) return null
+        let some_id = id.replace(/[^\d]/g, '')
+        let user = some_id ? (bot.users.cache.get(some_id)) || (await bot.users.fetch(some_id).catch(e=>null)): null
+        if(!strict && !user) user = (bot.users.cache.find(u => u.username.includes(id)) || null)
+        return user
+    }
+
+    /**
+     * Check if the provided usar has its DM channel open.
+     * @param bot - BDJS client.
+     * @param user - User to be checked.
+     * @returns {Promise<boolean>}
+     */
+    static async hasDM(bot: Bot, user: string | User): Promise<boolean> {
+        let u = user instanceof User ? user: (await this.getUser(bot, user, false))
+        if(!u) return false
+        const c = await u.send(' ').catch(err => err.code)
+        return c === 50007 ? false : true
     }
 }
