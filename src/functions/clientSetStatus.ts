a0f2db56fa6c13e8ff472634dc75e602ef177610
Cyberghxst <jeffreyvillatoroquezada@gmail.com>
2023-12-12 19:18:04 -0600

    $clientSetStatus error log

--- src/functions/clientSetStatus.ts
+++ src/functions/clientSetStatus.ts
@@ -1,5 +1,6 @@
 import { ActivityType, PresenceStatusData } from 'discord.js'
 import { BaseFunction } from '../structures/Function'
+import { inspect } from 'util'
 
 export default new BaseFunction({
     description: 'Set the presence for the client.',
@@ -63,15 +64,19 @@ export default new BaseFunction({
         if (!Object.keys(activityTypes).includes(type.toLowerCase()))
             throw new d.error(d, 'invalid', 'activity type', d.function?.name!)
 
-        await d.bot?.user.setPresence({
-            activities: [{
-                name: text,
-                state: type === 'custom' ? text : undefined,
-                type: activityTypes[type.toLowerCase()],
-                url
-            }],
-            afk: afk === 'true',
-            status: status as PresenceStatusData
-        })
+        try {
+            d.bot?.user.setPresence({
+                activities: [{
+                    name: text,
+                    state: type === 'custom' ? text : undefined,
+                    type: activityTypes[type.toLowerCase()],
+                    url
+                }],
+                afk: afk === 'true',
+                status: status as PresenceStatusData
+            })
+        } catch (e) {
+            throw new d.error(d, 'custom', inspect(e))
+        }
     }
 })
