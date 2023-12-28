1c738be5a1b8cc1217b3ebbba4f88eb59007e4e9
Cyberghxst <jeffreyvillatoroquezada@gmail.com>
2023-12-28 14:55:36 -0600

    Some util functions | Release 1.0.0-N

---
+++ src/functions/hyperlink.ts
@@ -0,0 +1,28 @@
+import { BaseFunction } from '../structures/Function'
+
+export default new BaseFunction({
+    description: 'Creates a HyperLink block.',
+    parameters: [
+        {
+            name: 'Text',
+            description: 'The text for the block.',
+            required: true,
+            resolver: 'String',
+            value: 'none'
+        },
+        {
+            name: 'URL',
+            description: 'The URL for the block.',
+            required: true,
+            resolver: 'String',
+            value: 'none'
+        }
+    ],
+    code: async function(d, [text, url]) {
+        if (text === undefined)
+            throw new d.error(d, 'required', 'Block Name', d.function!.name)
+        if (url === undefined)
+            throw new d.error(d, 'required', 'Block URL', d.function!.name)
+        return `[${text}](${url})`
+    }
+})
