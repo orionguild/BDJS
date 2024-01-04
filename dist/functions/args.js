ef7bf47f2a66f4500862ff359920c5f64196faca
Cyberghxst <jeffreyvillatoroquezada@gmail.com>
2024-01-04 10:22:38 -0600

    Fixed $args

--- dist/functions/args.js
+++ dist/functions/args.js
@@ -6,28 +6,33 @@ exports.default = new Function_1.BaseFunction({
     parameters: [
         {
             name: 'Index',
-            description: 'Argument index.',
-            required: true,
+            description: 'The index to get the argument.',
+            required: false,
             resolver: 'Number',
             value: 'none'
         },
         {
             name: 'End Index',
-            description: 'The slice end index.',
+            description: 'The index to slice message arguments.',
             required: false,
             resolver: 'Number',
             value: 'none'
         }
     ],
-    code: async function (d, [index = '0', end]) {
-        if (!['prefixed', 'unprefixed', 'always'].includes(d.commandType))
-            throw new d.error(d, 'disallowed', d.function?.name, 'Message-based events');
-        if (isNaN(Number(index)) || Number(index) < 0 || Number(index) > d.getEnvironmentVariable('__BDJS__ARGS__').length)
-            throw new d.error(d, 'invalid', 'Index', d.function?.name);
-        if (end && isNaN(Number(end)) || Number(end) < 0 || Number(end) > d.getEnvironmentVariable('__BDJS__ARGS__').length || Number(end) < Number(index))
-            throw new d.error(d, 'invalid', 'End Index', d.function?.name);
-        return end
-            ? d.getEnvironmentVariable('__BDJS__ARGS__').slice(Number(index), Number(end)).join(' ')
-            : d.getEnvironmentVariable('__BDJS__ARGS__').at(Number(index));
+    code: async (d, [index = '-1', endIndex]) => {
+        if (d.commandType !== 'always')
+            throw new d.error(d, 'disallowed', d.function.name, 'Message Context');
+        if (index === undefined)
+            throw new d.error(d, 'required', 'Index', d.function.name);
+        if (isNaN(Number(index)))
+            throw new d.error(d, 'invalid', 'Index', d.function.name);
+        if (endIndex && isNaN(Number(endIndex)))
+            throw new d.error(d, 'invalid', 'End Index', d.function.name);
+        const args = d.getEnvironmentVariable('__BDJS__ARGS__');
+        return endIndex
+            ? args.slice(Number(index), Number(endIndex)).join(' ')
+            : Number(index) === -1
+                ? args.join(' ')
+                : args[Number(index)];
     }
 });
